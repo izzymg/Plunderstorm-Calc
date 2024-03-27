198e160323ff9cb1e8cdbd29fb966f347a9b81e4
izzymg <izzymg@proton.me>
2024-03-27 15:22:10 +1300

    UX improvements

--- src/PlunderInput/PlunderInput.js
+++ src/PlunderInput/PlunderInput.js
@@ -4,7 +4,7 @@ export default function PlunderInput({ initialPlunder, onUpdate }) {
 
     return(
         <div className="inputWrap">
-            <label htmlFor='plunder'>YE PLUNDER</label>
+            <label htmlFor='plunder'>ENTER PLUNDER:</label>
             <input id='plunder' type='text' pattern="[0-9]*" inputMode="numeric" value={initialPlunder} onChange={onPlunderChange} placeholder='0'></input>
         </div>
     )
--- src/RenownInput/RenownInput.js
+++ src/RenownInput/RenownInput.js
@@ -6,7 +6,7 @@ export default function RenownInput({ initialRenown, onUpdate }) {
 
     return(
         <div className="inputWrap">
-            <label htmlFor='plunder'>YE RENOWN</label>
+            <label htmlFor='plunder'>ENTER RENOWN:</label>
             <input id='plunder' type='text' value={initialRenown} pattern="[0-9]*" inputMode="numeric" onChange={onRenownChange} placeholder='0'></input>
         </div>
     )
