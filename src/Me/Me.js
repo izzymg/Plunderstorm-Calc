832c931db3032a5c4acc297875797326e912a95f
izzymg <izzymg@proton.me>
2024-03-27 14:19:31 +1300

    update profile info

--- src/Me/Me.js
+++ src/Me/Me.js
@@ -1,7 +1,7 @@
 export default function Me() {
     return(
         <>
-            <span>Made by <a href="https://izzymg.dev/">izzymg.dev</a></span>
+            <span>Made by <a href="https://github.com/izzymg/">izzymg</a></span>
             <span className="small">no I didn't need to use react for this</span>
         </>
     )
